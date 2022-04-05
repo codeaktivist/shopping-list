ebd19741358df613bb7b2445f2efcd97491bafab
codeaktivist <codeaktivist@gmail.com>
2022-04-05 20:54:48 +0200

    Added users, multiple lists via cookie

--- app.py
+++ app.py
@@ -1,30 +1,73 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, make_response
+# from flask_session import Session
 
+# Configure app
 app = Flask(__name__)
 
+# Configure session and cookie
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+
+# Global name
+name = ""
+
+# Dictionary to store names:items{}
+names = {}
+
+# Dictionary to store items
 items = {}
 
+# Routes
 @app.route("/")
 def index():
-    return render_template("index.html", items=items)
+    global name
+    # Get name from session cookie
+    name = request.cookies.get("name")
+    # Force login
+    if name == "" or name == None:
+        return redirect("/login")
+    # Load existing items or create new items{}
+    if not name in names:
+        names[name] = {}
+    
+    return render_template("index.html", items=names[name], name=name)
+
+@app.route("/login")
+def login():
+    global name
+    # Check if name gets passed
+    name = request.args.get("name", "")
+    if not name:
+        return render_template("login.html")
+    else:
+        res = make_response(redirect("/"))
+        res.set_cookie("name", name)
+        return res
+
+@app.route("/logout")
+def logout():
+    global name
+    name = ""
+    res = make_response(redirect("/login"))
+    res.delete_cookie("name")
+    return res
 
 @app.route("/add")
 def add():
+    global name
     item = request.args.get("item", "default")
-    if item in items:
-        items[item] += 1
+    if item in names[name]:
+        names[name][item] += 1
     else:
-        items[item] = 1
+        names[name][item] = 1
     print(f"added: {item}")
     return redirect("/")
-    # return render_template("add.html", item=item, items=items)
 
 @app.route("/remove")
 def remove():
+    global name
     item = request.args.get("item", "default")
-    if item in items:
-        del items[item]
+    if item in names[name]:
+        del names[name][item]
     print(f"removed: {item}")
     return redirect("/")
-    # return render_template("remove.html", item=item, items=items)
-
